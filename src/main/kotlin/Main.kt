76ddb19dc7b69d8ad0e7cac5414571c29858090c
eMobilis <you@example.com>
2023-04-19 15:31:33 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+
+    var name = "peter"
+    var age = 20
+    println(name)
+    println(age)
+
+    var G = 45
+    var H = 30
+    var sum = G + H
+    println(sum)
+
+    var P = 76
+    var C = 42
+    var SUM = P - C
+    println(SUM)
+
+
+
+
+}
