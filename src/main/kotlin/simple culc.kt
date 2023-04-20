9468c249291a05421481339333d73bced5e719c6
eMobilis <you@example.com>
2023-04-20 14:42:11 +0300

    my project

--- src/main/kotlin/simple culc.kt
+++ src/main/kotlin/simple culc.kt
@@ -6,4 +6,21 @@ fun main(args: Array<String>) {
     println("sum = num1 + num2")
     var sum = num1 + num2
     println(sum)
+
+    println("___Enter Num3___")
+    var num3 = readln()!!.toInt()
+    println("___Enter Num4___")
+    var num4 = readln()!!.toInt()
+    println("sub = num3 - num4")
+    var sub = num3 - num4
+    println(sub)
+
+    println("___Enter Num5___")
+    var num5 = readln()!!.toInt()
+    println("___Enter Num6___")
+    var num6 = readln()!!.toInt()
+    println("Devide = num5 / num6")
+    var Divide = num5 / num6
+    println(Divide)
+
 }
